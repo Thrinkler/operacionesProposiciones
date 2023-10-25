4fc4bc23f77249c4e49df33c181c645a7df35e3a
ThrinklerAzra <aeshadow0935@gmail.com>
2023-10-25 14:00:23 -0600

    0.06

--- opelog.py
+++ opelog.py
@@ -14,7 +14,7 @@ def dex(p,q):                           ## dos variables din() y dex() para emul
 
 n = int(input("numero de variables: "))
 l = 2**n
-v = [[i>>j&1 for j in range(n-1,-1,-1)] for i in range(l-1, -1, -1)]    ## una version corta para hacer una combinatoria de verdaderos y falsos empezando
+linicial = [[i>>j&1 for j in range(n-1,-1,-1)] for i in range(l-1, -1, -1)]    ## una version corta para hacer una combinatoria de verdaderos y falsos empezando
                                                                         ## todos verdaderos y terminando todos falsos
 menu = """
 # NOT = not
@@ -30,10 +30,10 @@ ejemplo: (not r[0]) or (r[1])
 option = "y"
 while (option == "y")|(option == "s"):
     print(menu)
-    for row in v: print(row)
+    for row in linicial: print(row)
     operacion = input("dime la operacion ")         ##Nuestro plan es hacer que se corra un codigo al final, por lo que nombramos la variable respuesta
                                                             ## y usamos el input para correr el programa. 
-    lista = [eval(operacion) for r in v]                     ## y hacemos una lista con todos los resultados de la operacion.
+    lista = [eval(operacion) for r in linicial]                     ## y hacemos una lista con todos los resultados de la operacion.
 
     if all(i== lista[0] for i in lista):            ## comparamos si todos los valores son iguales en la lista
         if lista[0] == True:
@@ -43,16 +43,16 @@ while (option == "y")|(option == "s"):
     else:
         print("Es una contingencia\n")              ## si los valores no son todos iguales, se llama contingente.
 
-    for f in range(len(v[0])):
+    for f in range(len(linicial[0])):
         print(f)
-        if all(v[n][f]==i for n,i in enumerate(lista)):             ## Comparamos nuestra nueva lista con los valores pasados y vemos si se parecen.
+        if all(linicial[n][f]==i for n,i in enumerate(lista)):             ## Comparamos nuestra nueva lista con los valores pasados y vemos si se parecen.
             print("esta expresion es igual a la lista "+str(f+1))
 
-    for n,k in enumerate(v):
-        k.append(lista[n])                          ## Finalmente almacenamos la lista en nuestra primera lista para hacer mas trabajos con ella
+    for n,valores in enumerate(linicial):
+        valores.append(lista[n])                          ## Finalmente almacenamos la lista en nuestra primera lista para hacer mas trabajos con ella
 
 
-    for row in v: print(row)
+    for row in linicial: print(row)
     option = input("\nQuieres hacer una segunda operacion\nusando la lista anterior y los conjuntos? Y/N\n").lower()  ## si pone y,Y,s o S, repetimos el codigo.
 
 
